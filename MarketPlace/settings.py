e3419602559f7ba87b5d7f3e732f12af39842043
josevilamourac <josevilamourac@gmail.com>
2024-05-12 23:43:43 +0100

    new

--- MarketPlace/settings.py
+++ MarketPlace/settings.py
@@ -55,15 +55,16 @@ ROOT_URLCONF = 'MarketPlace.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'MarketPlaceAPP/templates']
-        ,
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
-    'context_processors': [
-        # Outros context_processors aqui...
-        'MarketPlaceAPP.context_processors.saldo_global',  # Adicione o seu contexto de processador aqui
-    ],
-},
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
     },
 ]
 
